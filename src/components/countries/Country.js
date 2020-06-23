c7a5866a5d3439e5e82bc8225095d08634fa1111
gustavobgt <gustavo_bgt97@hotmail.com>
2020-06-23 05:36:15 -0300

    Converting Class to Functional Components

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import css from './countries.module.css';
 
-export default class Country extends Component {
-  render() {
-    const { name, flag } = this.props.country;
+export default function Country({ country }) {
+  const { name, flag } = country;
 
-    return (
-      <div className={`${css.country} ${css.border}`}>
-        <img className={css.flag} src={flag} alt={name} />
-        <span className={css.countryName}>{name}</span>
-      </div>
-    );
-  }
+  return (
+    <div className={`${css.country} ${css.border}`}>
+      <img className={css.flag} src={flag} alt={name} />
+      <span className={css.countryName}>{name}</span>
+    </div>
+  );
 }
